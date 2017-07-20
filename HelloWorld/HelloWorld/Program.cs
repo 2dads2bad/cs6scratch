6d397d4b7fa007aa7921bc79a5fdfd555b0abe09
2dads2bad <30288677+2dads2bad@users.noreply.github.com>
2017-07-20 16:56:39 -0700

    Add HelloWorld
    My very first Visual Studio project. Let ME tell YOU, it was a long, hard road getting here. Blah blah blah.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What would you like to say?");
+            string say = Console.ReadLine();
+            if (say == "Hello World")
+            {
+                Console.WriteLine("That's what I was thinking!");
+            }
+            else
+            {
+                Console.WriteLine('\"'+say + "\" to you, too.");
+            }
+        }
+    }
+}
